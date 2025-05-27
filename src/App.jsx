3b856619b27190c433759731e4a234543580e29d
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-05-27 10:39:13 +0100

    init et setup du projet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='flex justify-center items-center min-h-screen bg-blue-900'>
+			<h1 className='text-3xl font-bold underline text-white'>
+				Landing Page BuilderMania
+			</h1>
+		</div>
+	)
+}
+
+export default App
